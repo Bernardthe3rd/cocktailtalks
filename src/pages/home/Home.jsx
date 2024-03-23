5c731a78ca08ffb120cc8203c1457511da105ae2
Bernardthe3rd <bammeijer@outlook.com>
2024-03-23 13:01:34 +0100

    changed to the right branche and started styling the home page upperpart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,43 @@
+import logo from "./assets/Logo.png";
+import homeimg from "./assets/imgHomepage.jpg";
+import "./home.css"
+import {Link, NavLink} from "react-router-dom";
+
 
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <header>
+                <nav className="container">
+                    <ul className="navbar-container">
+                        <li><NavLink to="/catalog" className="button-link">catalog</NavLink></li>
+                        <li><NavLink to="/about">about</NavLink></li>
+                        <li>
+                            <span className="img-wrapper-logo">
+                                <img src={logo} alt="logo cocktailtalks"/>
+                            </span>
+                        </li>
+                        <li><NavLink to="/randomizer">randomizer</NavLink></li>
+                        <li><NavLink to="/login">account</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="container">
+                <article className="home--article__container">
+                    <div className="home--content__container">
+                        <h1>PHENOMENAL COCKTAIL. PHENOMENAL COMMUNITY.</h1>
+                        <h3>From anywhere in the world,</h3>
+                        <h3>For all cocktail fans here at CocktailTalks</h3>
+                        <Link to="/about">about</Link>
+                    </div>
+                    <span className="img-wrapper-homeimg">
+                        <img src={homeimg} alt="cocktail glasses"/>
+                    </span>
+                </article>
+            </main>
 
         </>
     );
-};
-
+}
 export default Home;
