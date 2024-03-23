4842b4d40e15e249aadc8a1d24c00ec9e67db0f4
Bernardthe3rd <bammeijer@outlook.com>
2024-03-23 17:21:34 +0100

    finished styling login/register page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,29 @@
-
+import "./login.css"
+import {Link} from "react-router-dom";
 const Login = () => {
     return (
         <>
-            <h1>Login</h1>
+            <main className="container">
+                <div className="main--container__outer">
+                    <h2>Welcome (back)!</h2>
+                    <h3>Ready to explore some cocktails?</h3>
+                    <form className="form-container">
+                        <label htmlFor="field-email">
+                            Email:
+                            <input type="text" id="field-email" name="email" className="input-text-field"/>
+                        </label>
+                        <label htmlFor="field-password">
+                            Password:
+                            <input type="text" id="field-password" name="password" className="input-text-field"/>
+                            <a href="mailto:benjaminmeijer1@gmail.com">Forgot password?</a>
+                        </label>
+                        <div className="login--div__button">
+                            <button type="button" className="button-function">log in</button>
+                            <button type="button" className="button-function">register</button>
+                        </div>
+                    </form>
+                </div>
+            </main>
 
         </>
     );
