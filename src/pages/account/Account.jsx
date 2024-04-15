06f6d380a5a1c92ba2a1e19c89216169ad8ef652
Bernardthe3rd <bammeijer@outlook.com>
2024-04-15 21:18:44 +0200

    made it possible that the account page only fetches the cocktails which id's are in an array, array will later be delivered by user info in the backend

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,39 +1,78 @@
 import "./account.css"
 import reactlogo from "/src/assets/react.svg";
 import ProductCardReview from "../../components/product-card-review/ProductCardReview.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 const Account = () => {
     const [disable, toggleDisable] = useState(false);
     const [cocktailInfo, setCocktailInfo] = useState([]);
+    const [loading, toggleLoading] = useState(false);
+    const [error, toggleError] = useState(false);
 
     function handleClick () {
         toggleDisable(!disable);
         //post request met account meesturen
+        //opslaan van cijfer en feedback text in account
     }
 
-    async function fetchFavoriteCocktails () {
-        try {
-            const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`)
-            console.log(response.data.drinks);
-            setCocktailInfo(response.data.drinks);
-        } catch (e) {
-            console.error(e)
+
+    useEffect(() => {
+        const userCocktails = [15346, 14029, 178318]; //hier komt de array met cocktail ids van de gebruiker
+        const controller = new AbortController();
+
+        async function fetchFavoriteCocktails () {
+            toggleLoading(true);
+            let newArray = []
+            for (let i = 0; i < userCocktails.length; i++) {
+                try {
+                    toggleError(false);
+                    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${userCocktails[i]}`, {//nummer vervangen door ID of naam drink uit userinfo
+                        signal: controller.signal,
+                    })
+                    newArray.push(response.data.drinks)
+                } catch (e) {
+                    console.error(e)
+                    toggleError(true);
+                }
+                setCocktailInfo(newArray);
+            }
+            toggleLoading(false);
         }
-    }
+        void fetchFavoriteCocktails();
+
+        // return function cleanup() {
+        //     controller.abort();
+        // } kijken hoe werkend te krijgen gezien hij direct de fetch cancelled..
+
+    }, []);
+
+    const control = cocktailInfo.map((item) => {
+        return item[0]
+    })
+    console.log(control)
 
     return (
         <>
             <main className="container">
-                <div className="main--container__outer">
+                {loading && <p className="loading">Loading...</p>}
+                {error ? <p className="error">Er is iets misgegaan, klik op het logo om naar Home te gaan en kom later terug.</p> :
+                <div className="container__div">
                     <h2>Your favorites</h2>
-                    {cocktailInfo.map((cocktail) => {
-                        return <ProductCardReview key={cocktail.idDrink} source={cocktail.strDrinkThumb} alt="thumbnail cocktail" nameProduct={cocktail.strDrink} disable={disable} clicked={handleClick}/>
+                    {control.map((cocktail) => {
+                        return <ProductCardReview
+                            key={cocktail.idDrink}
+                            source={cocktail.strDrinkThumb}
+                            alt="thumbnail cocktail"
+                            nameProduct={cocktail.strDrink}
+                            disable={disable}
+                            clicked={handleClick}
+                        />
 
                     })}
-                    <ProductCardReview source={reactlogo} alt="picture cocktail" nameProduct="naam vd cocktail 2" disable={disable} clicked={fetchFavoriteCocktails}/>
+                    <ProductCardReview source={reactlogo} alt="picture cocktail" nameProduct="naam vd cocktail 2" disable={disable} clicked={handleClick}/>
                 </div>
+                }
             </main>
         </>
     );
