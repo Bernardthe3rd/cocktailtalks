f1bafce716fddb419237f4cf4c1bd78d78ceed04
Bernardthe3rd <bammeijer@outlook.com>
2024-04-20 17:20:35 +0200

    put a little story and further dedication into the about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -10,7 +10,19 @@ const About = () => {
                     <div className="about__div-box">
                         <h3>Our story - How CocktailTalk started</h3>
                         <article className="about__article">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque aut consequuntur deserunt dolor doloremque dolores doloribus ducimus eaque eius eos error esse fugiat fugit hic iste magni mollitia optio possimus quasi quis quo, quod sequi suscipit tenetur ullam veritatis vero voluptates voluptatibus? Corporis deleniti est fugiat hic id illo odit quia! Aliquid autem consequatur corporis dolores est illo magnam maiores maxime mollitia? Ad aperiam cum dolor dolores est harum hic, laborum laudantium libero natus nulla obcaecati quisquam saepe similique ut vel vero? Ab adipisci aspernatur deleniti dolorem ea enim ex, expedita facere iste iusto necessitatibus nemo nesciunt nihil, nulla odio porro quaerat quas qui repellat repellendus sint velit veritatis voluptates. Ad adipisci enim, eum fuga qui quod sed temporibus ullam! Adipisci aliquid dolorem enim expedita iste nostrum sit soluta sunt. A adipisci aliquid animi distinctio doloribus dolorum eos et ex expedita harum, in ipsam laboriosam laborum libero magnam minus nam neque nihil nostrum nulla odio odit porro praesentium provident quas quibusdam quis reiciendis sit suscipit tempora totam ut veniam vero voluptate voluptatem voluptatibus voluptatum? Ab aspernatur dolores nam placeat quidem quo ratione recusandae reiciendis saepe voluptatum. Autem obcaecati, perspiciatis praesentium suscipit temporibus voluptatibus! Aliquam aperiam architecto blanditiis, debitis dolore earum est expedita facere fugit impedit iste laboriosam maxime, odio perferendis quas rem saepe sequi tempore veritatis voluptate? Nulla, sapiente, ullam. Accusamus aliquam aperiam beatae corporis, ducimus error eveniet ex illo inventore ipsum iste iusto laudantium maiores necessitatibus non, nostrum nulla obcaecati odit perferendis, repellat sapiente sint unde voluptatum?</p>
+                            <h4>Welcome to CocktaisTalks</h4>
+                            <p>Here's a little story about a young guy who loves Cocktails but always had the same questions everytime he wanted one.</p>
+                            <h4>Do you also have the same questions as:</h4>
+                            <ul>
+                                <li>I would like to try a new cocktail, but which one?</li>
+                                <li>How do I make a certain cocktail and what kind of ingredients do I need?</li>
+                                <li>Which cocktails did I even had in the past?</li>
+                                <li>And did I even liked them?</li>
+                            </ul>
+                            <p>Sounds familiair? Then stop hesitating and join our community! </p>
+                            <p>Because here at CocktailTalks we welcome every cocktail enthousiast and love to share our passion, ideas and feedback.</p>
+                            <p>Are you ready, really ready to explore so many cocktails you every dreamed of?</p>
+                            <p>Got excited yet? Click on the button below and come join our CocktailTastic Community!</p>
                             <ButtonLink path="/login" text="join us"/>
                         </article>
                     </div>
